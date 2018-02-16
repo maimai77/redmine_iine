15db1a5c9ce8f7d95dce2a3b214a1a25b57050ec
maimai77 <kohta0707@gmail.com>
2018-02-16 14:53:50 +0900

    change hook point

--- lib/redmine_iine/hook.rb
+++ lib/redmine_iine/hook.rb
@@ -2,11 +2,6 @@
 
 module RedmineIine
   class Hook < Redmine::Hook::ViewListener
-    def view_layouts_base_content(context = {})
-      controller = context[:controller]
-      if controller.controller_name == 'issues' && controller.action_name == 'show'
-        controller.render partial: 'redmine_iine/hooks/view_layouts_base_content'
-      end
-    end
+    render_on :view_issues_show_description_bottom, partial: 'redmine_iine/hooks/view_issues_show_description_bottom'
   end
 end
